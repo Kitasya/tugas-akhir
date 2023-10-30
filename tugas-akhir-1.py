b0b2cde55f33fb81bd0bf503d48384be7b1900d9
syasya <nikitasyagita.laurensia2018@gmail.com>
2023-10-30 23:43:14 +0700

    Buat init

--- tugas-akhir-1.py
+++ tugas-akhir-1.py
@@ -1,1 +1,10 @@
-print("hello")
+import pygame
+
+class Tiktekto:
+    def __init__(self):
+        pygame.init()
+        
+        self.size = width, height = 640, 320
+        self.background = (224, 241, 244)
+        self.color_game = (0, 55, 61)
+        self.screen = pygame.display.set_mode(self.size)
