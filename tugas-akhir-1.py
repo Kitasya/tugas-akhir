c191a5784483c7b261358d1870acf4464d065ef1
syasya <nikitasyagita.laurensia2018@gmail.com>
2023-11-01 16:57:38 +0700

    buat mau start nya

--- tugas-akhir-1.py
+++ tugas-akhir-1.py
@@ -8,4 +8,7 @@ class Tiktekto:
         self.background = (224, 241, 244)
         self.color_game = (0, 55, 61)
         self.screen = pygame.display.set_mode(self.size)
-        Halo
+
+    def main_game(self):
+        while True:
+            self.screen.fill(self.background)
